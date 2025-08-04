7b0835dcd4d5208f9368d1ae9631168b6aca088b
ketan <ketansingla7988@gmail.com>
2025-08-04 10:44:08 +0530

    FS Module

---
+++ 10-fs-module.js
@@ -0,0 +1,10 @@
+//SYNCHRONOUS METHODS
+
+const {readFileSync, writeFileSync} = require("fs");
+//const fs = require("fs");
+
+
+const first = readFileSync("./content/first.txt", "utf-8");
+const second = readFileSync("./content/second.txt", "utf-8");
+
+writeFileSync("./content/result.txt", `Stored result is: ${first}, ${second}`);
