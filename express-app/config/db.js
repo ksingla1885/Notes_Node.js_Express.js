a107f9cfa1fe9bc217a96330b7f749aad5e3d198
Ketan Singla <157493409+ksingla1885@users.noreply.github.com>
2025-09-11 00:24:45 +0530

    Update db.js

--- express-app/config/db.js
+++ express-app/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    const MONGODB_URL = "mongodb+srv://chillmodeon123:restapi@cluster0.ov5ejxn.mongodb.net/express-app";
+    const MONGODB_URL = "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.ov5ejxn.mongodb.net/express-app";
 
     await mongoose.connect(MONGODB_URL).then(() => {
         console.log("Connected to MongoDB");
